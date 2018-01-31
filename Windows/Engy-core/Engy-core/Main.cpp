e1597167da31968b6f2cf0547ba894092147d18b
Aegis223 <ptnaung@cpp.edu>
2018-01-31 09:07:40 -0800

    Added Vec2-Vec4 structures

--- Windows/Engy-core/Engy-core/Main.cpp
+++ Windows/Engy-core/Engy-core/Main.cpp
@@ -1,15 +1,23 @@
 #include <iostream>
 
 #include "src/graphics/Window.h"
+#include "src/maths/Maths.h"
 
 using namespace engy;
 using namespace graphics;
+using namespace maths;
 
 int main(int argc, char* args[]) 
 {
 	Window* u_Window = new Window(1280, 720, "TEST");
 
 	glClearColor(0.2f, 0.3f, 0.8f, 1.0f);
+
+	vec2 a(2.0f, 3.0f);
+	vec2 b(3.0f, 2.0f);
+
+	std::cout << a.add(b) << std::endl;
+	std::cout << b << std::endl;
 
 	while (!u_Window->closed()) {
 		u_Window->clear();
